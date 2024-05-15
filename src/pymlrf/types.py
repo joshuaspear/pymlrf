7301811e6f795ddb9e4f4651ef3bd6ff07ab6059
Joshua Spear <josh.spear9@gmail.com>
2024-05-15 12:10:00 +0100

    made types runtime checkable

--- src/pymlrf/types.py
+++ src/pymlrf/types.py
@@ -1,11 +1,11 @@
-from typing import Protocol, Tuple, Literal, Any
+from typing import Protocol, Tuple, Literal, Any, runtime_checkable
 import torch
 from torch.utils.data import DataLoader
 import logging
 
 from .Structs.torch.Dataset import DatasetOutput
 
-
+@runtime_checkable
 class TrainSingleEpochProtocol(Protocol):
         
     def __call__(
@@ -19,7 +19,7 @@ class TrainSingleEpochProtocol(Protocol):
         ) -> Tuple[torch.Tensor,torch.Tensor]:
         ...
 
-
+@runtime_checkable
 class ValidateSingleEpochProtocol(Protocol):
     
     def __call__(
@@ -31,7 +31,7 @@ class ValidateSingleEpochProtocol(Protocol):
         ) -> Tuple[torch.Tensor,torch.Tensor]:
         ...
 
-
+@runtime_checkable
 class GenericDataLoaderProtocol(Protocol):
     
     def __iter__(self)->"GenericDataLoaderProtocol":
@@ -40,7 +40,7 @@ class GenericDataLoaderProtocol(Protocol):
     def __next__(self)->DatasetOutput:
         ...
 
-
+@runtime_checkable
 class CriterionProtocol(Protocol):
 
     def __call__(self, *args: Any, **kwds: Any) -> torch.Tensor:
